aa9c3cfd4ee654e59a9a23872378bfaad49f7235
woowabrie <buyong.jo@woowahan.com>
2026-04-26 20:03:14 +0900

    feat: setup for 8th

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -5,16 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
+import static org.hamcrest.Matchers.is;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
     @Test
-    void 일단계() {
+    void 예약_조회() {
         RestAssured.given().log().all()
-                .when().get("/")
+                .when().get("/reservations")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body("size()", is(0)); // 아직 생성 요청이 없으니 0개
     }
 
 }
